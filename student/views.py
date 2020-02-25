fb9ae6c6b9c161def41ee10aa1a4feb9a12fc874
Nikolaos IOANNOU <nikolaos.ioannou.e@thalesdigital.io>
2020-02-25 17:28:32 +0100

    add view in the student app

--- student/views.py
+++ student/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+def student_show(request):
+    x = []
+    for i in range(10):
+        x.append(i)
+    return HttpResponse("<h1>DataFlair Django Tutorials</h1>The Digits are {0}".format(x))
